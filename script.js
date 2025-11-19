725eafbd101d774820cef2a0d95561e1b974254f
matsepus <saghaugmats@gmail.com>
2025-11-19 16:20:41 +0100

    fikse db som skal lagres i

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ async function sendInput() {
   popupOverlay.style.display = "none";
   console.log("Sending input to Firestore...");
   try {
-    const docRef = await addDoc(collection(db, currentUserCred.email + " - albumCards"), {
+    const docRef = await addDoc(collection(db, currentUserCred.user.email + " - albumCards"), {
       albumName: alnI.value,
       artistName: artnI.value,
       coverURL: coverI.value,
