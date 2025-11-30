92877087b772dd1824e4c999bcb091efc069d594
matsepus <saghaugmats@gmail.com>
2025-11-30 15:47:39 +0100

    fikse fikse

--- suggestion/script.js
+++ suggestion/script.js
@@ -28,7 +28,7 @@ onAuthStateChanged(auth, (user) => {
         currentUserCred = { user };
         console.log("Logged in:", user.email, "motherfucker ^_^");
     } else {
-        window.open("aaad.icu", "_self")
+        window.open("https://aaad.icu", "_self")
     }
 });
 
