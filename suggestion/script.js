f6ff975db4ae32fb02e7b07d4914a08d88e5a1a7
matsepus <saghaugmats@gmail.com>
2025-11-30 15:49:35 +0100

    www

--- suggestion/script.js
+++ suggestion/script.js
@@ -28,7 +28,7 @@ onAuthStateChanged(auth, (user) => {
         currentUserCred = { user };
         console.log("Logged in:", user.email, "motherfucker ^_^");
     } else {
-        window.open("https://aaad.icu", "_self")
+        window.open("https://www.aaad.icu", "_self")
     }
 });
 
